89327ddc3e5694112c3c489b2d7c2c7ac28ba14a
Yeraybote <yeray.bote@ikasle.egibide.org>
2022-11-09 10:01:46 +0100

    Layout perfil

---
+++ JS/perfil.js
@@ -0,0 +1,8 @@
+document.getElementById("editar").addEventListener("click",edit);
+
+function edit() {
+    let inputs = document.getElementsByClassName("inputs");
+    for (let x = 0; x < inputs.length; x++) {
+        inputs[x].disabled = false;
+    }
+}
