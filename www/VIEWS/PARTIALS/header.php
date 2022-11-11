ecc96320c18391b32cb80adacdb133b086d494fd
Alba <alba.alonso@ikasle.egibide.org>
2022-11-11 12:08:08 +0100

    index: login + partials

--- www/VIEWS/PARTIALS/header.php
+++ www/VIEWS/PARTIALS/header.php
@@ -5,6 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet" href="../../CSS/index.css" />
+        <link rel="stylesheet" href="../CSS/yeti.css"/>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"/>
         <link  href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
         <link  rel="shortcut icon" href="../../RECURSOS/ASSETS/favicon.png"/>
@@ -40,4 +41,4 @@
             </div>
                 
         
-        <div id="main">
+            <div id="main">
