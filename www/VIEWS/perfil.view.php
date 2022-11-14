8435ae41d0e04591e80524acf2b3b59cfe9e4dbe
Alba <alba.alonso@ikasle.egibide.org>
2022-11-14 09:47:25 +0100

    perfil con partials

--- www/VIEWS/perfil.view.php
+++ www/VIEWS/perfil.view.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Perfil</title>
-    <link rel="stylesheet" type="text/css" href="../CSS/perfil.css">
-</head>
-<body>
+<div id="perfil">
     <div class="datos">
         <form method="post">
             <div class="izq">
@@ -30,5 +21,4 @@
         </form>
     </div>
     <script src="../JS/perfil.js"></script>
-</body>
-</html>
+</div>
