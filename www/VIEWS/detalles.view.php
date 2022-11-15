1a47d37c2495f3b18cda81c61f18d3f8ed41c6ce
Alba <alba.alonso@ikasle.egibide.org>
2022-11-15 21:34:30 +0100

    layout publicacion respuestas

--- www/VIEWS/detalles.view.php
+++ www/VIEWS/detalles.view.php
@@ -68,5 +68,26 @@
         </div>
     </div>
 
+    <div class="publicarRespuesta">
+        <div class="recuadroFormu datos">
+                <form method="post" action="">
+                    <div class="izq">
+                        <h2>RESPONDER</h2><br>
+                        <label for="detalle">Detalle:</label>
+                        <br>
+                        <textarea class="inputs" name="detalle" id="detalle" cols="100" rows="10" maxlength="500" ></textarea>
+                        <br> 
+                        <b style="color:red" id="detalleIncorrecto" hidden>Detalle no puede estar vacio</b>
+                        <br><br>
+                    </div> 
+                    <div class="der end">
+                        <label for="archivo">Subir archivo</label>
+                        <input class="inputs" type="file" name="archivo" id="archivo">
+                    </div> 
+                    <input class="inputs" type="submit" value="Responder" id="preguntar">
+                </form>
+            </div>
+    </div>
+   
 
     
