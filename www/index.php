53779d549641a3f5c3673b199afda5f0861a0353
Alba <alba.alonso@ikasle.egibide.org>
2022-11-15 18:26:00 +0100

    quitar scroll en x

--- www/index.php
+++ www/index.php
@@ -3,54 +3,53 @@
     require ('./db_functions.php');
     require('VIEWS/PARTIALS/header.php');
 ?>
-
         
 <?php
-// SI NO TIENE CREADA UNA SESIÓN...
-if (!isset($_SESSION['id_usu'])) {
-    require('VIEWS/login.view.php');
-    
-    // Inicio de sesión
-    if (isset($_POST['email']) && isset($_POST['pswd'])) {
-        $inicio = userLogin($_POST['email'],$_POST['pswd']); // Si es TRUE es correcto
-        if ($inicio) {
-            echo '<p style="color:green">Inicio de sesión hecho correctamente</p>';
-        } else echo '<p style="color:red">Contraseña o email no validos</p>';
-    }
+    // SI NO TIENE CREADA UNA SESIÓN...
+    if (!isset($_SESSION['id_usu'])) {
+        require('VIEWS/login.view.php');
+        
+        // Inicio de sesión
+        if (isset($_POST['email']) && isset($_POST['pswd'])) {
+            $inicio = userLogin($_POST['email'],$_POST['pswd']); // Si es TRUE es correcto
+            if ($inicio) {
+                echo '<p style="color:green">Inicio de sesión hecho correctamente</p>';
+            } else echo '<p style="color:red">Contraseña o email no validos</p>';
+        }
 
-    // Registrarse
-    if (isset($_POST['remail']) && isset($_POST['rpswd']) && isset($_POST['rnombre'])) {
-        $registro = userRegistration($_POST['rnombre'],$_POST['remail'],$_POST['rpswd']); // Si es TRUE es correcto
-        if ($registro) {
-            echo '<p style="color:green">Registro hecho correctamente</p>';
-        } else echo '<p style="color:red">Fallo al registrar</p>';
-    }
-} else { // SI TIENE UNA SESIÓN INICIADA
-    if (isset($_GET['accion'])) {
-        switch ($_GET['accion']) {
-            case 'preguntas':
-                $dbh = connect();
-                $infousuario = getUsuario($dbh);
-                require('VIEWS/publicarPregunta.view.php');
-                break;
-            case 'perfil':
-                $dbh = connect();
-                $infousuario = getUsuario($dbh);
-                require('VIEWS/perfil.view.php');
-                break;
-            case 'cerrarsesion':
-                cerrarSesion();
-                require('VIEWS/login.view.php');
-                break;
-            default:
-                require('VIEWS/visualizarPreguntas.view.php');
-                break;
+        // Registrarse
+        if (isset($_POST['remail']) && isset($_POST['rpswd']) && isset($_POST['rnombre'])) {
+            $registro = userRegistration($_POST['rnombre'],$_POST['remail'],$_POST['rpswd']); // Si es TRUE es correcto
+            if ($registro) {
+                echo '<p style="color:green">Registro hecho correctamente</p>';
+            } else echo '<p style="color:red">Fallo al registrar</p>';
+        }
+    } else { // SI TIENE UNA SESIÓN INICIADA
+        if (isset($_GET['accion'])) {
+            switch ($_GET['accion']) {
+                case 'preguntas':
+                    $dbh = connect();
+                    $infousuario = getUsuario($dbh);
+                    require('VIEWS/publicarPregunta.view.php');
+                    break;
+                case 'perfil':
+                    $dbh = connect();
+                    $infousuario = getUsuario($dbh);
+                    require('VIEWS/perfil.view.php');
+                    break;
+                case 'cerrarsesion':
+                    cerrarSesion();
+                    require('VIEWS/login.view.php');
+                    break;
+                default:
+                    require('VIEWS/visualizarPreguntas.view.php');
+                    break;
+            }
+        }
+        else {
+            require('VIEWS/visualizarPreguntas.view.php');
         }
     }
-    else {
-        require('VIEWS/visualizarPreguntas.view.php');
-    }
-}
 ?>
 
 <?php require('VIEWS/PARTIALS/footer.php') ?>
