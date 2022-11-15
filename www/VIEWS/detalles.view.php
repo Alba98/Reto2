feef2f9594893050d54eb386e16ef28bd8a46ca2
Alba <alba.alonso@ikasle.egibide.org>
2022-11-15 21:09:52 +0100

    layout visualizacion respuestas

--- www/VIEWS/detalles.view.php
+++ www/VIEWS/detalles.view.php
@@ -33,3 +33,40 @@
             </div>
         </div>
     </div>
+
+    <div class="respuestas">
+        <div class="respuesta">
+            <div class="votacion">
+                <a class="like" href="#1"><i class="fa-solid fa-sort-up"></i></a>
+                <b id="votos" class="votos">VOTOS</b>
+                <a class="like" href="#2"><i class="fa-solid fa-sort-down"></i></a>
+            </div>
+            <div class="user">
+                <h3 id="titulousuario">Usuario</h3>
+                <img class="perfil" src="../RECURSOS/IMAGES/user.png" alt="Foto de perfil">
+                <form>
+                    <p class="clasificacion">
+                    <input id="radio1" type="radio" name="estrellas" value="5">
+                    <label for="radio1">★</label>
+                    <input id="radio2" type="radio" name="estrellas" value="4">
+                    <label for="radio2">★</label>
+                    <input id="radio3" type="radio" name="estrellas" value="3">
+                    <label for="radio3">★</label>
+                    <input id="radio4" type="radio" name="estrellas" value="2">
+                    <label for="radio4">★</label>
+                    <input id="radio5" type="radio" name="estrellas" value="1">
+                    <label for="radio5">★</label>
+                    </p>
+                </form>
+            </div>
+            <div class="info">
+                <b id="votos" class="votos">RESPUESTA</b>
+                <div class="descripcion">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi a ipsa aperiam, modi sapiente non praesentium aut ipsum voluptate. Sequi consectetur repellat hic veritatis quidem maiores minus qui architecto beatae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, rerum aperiam! Quo maxime esse iusto itaque aliquam officiis, voluptate placeat debitis amet voluptates consequuntur a inventore quae veniam rerum similique.
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    
