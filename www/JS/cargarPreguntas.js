cc3466fcd0e5c3920a446dc505fba3ebd15c2d93
Ania <aniasura.khiari@ikasle.egibide.org>
2022-11-17 13:43:55 +0100

    fetch cargarPreguntas

--- www/JS/cargarPreguntas.js
+++ www/JS/cargarPreguntas.js
@@ -19,17 +19,87 @@
  let cargarPreguntas = async() =>{
 
     try {
-        let pregunta = '';
+        let pregunta = ('../PHP/pregunta.php?id_preg='+id_preg, 
+        {
+            method: 'POST',
+            async: true,
+            headers: { 'Content-Type': 'application/json;charset=utf-8' },
+            body: JSON.stringify({"id_preg": id_preg})
+        });
+        console.log(pregunta);
+        // Si la respuesta es correcta
+           if(pregunta.status === 10){
+            const datos = await pregunta.json(); //convertir datos JSON
+            
+            let preguntas = '';
+            datos.results.forEach(pregunta => {
+                preguntas += `
+                <div class='preguntas'>
+                <div class='user'>
+                    <h2 class='titulousuario' id='titulousuario'>$preg->usuario</h2>
+                    <img class='perfil' src='../RECURSOS/IMAGES/user.png' alt='Foto de perfil'>
+                    <form>
+                        <p class='clasificacion'>
+                          <input id='radio1' type='radio' name='estrellas' value='5'>
+                          <label for='radio1'>★</label>
+                          <input id='radio2' type='radio' name='estrellas' value='4'>
+                          <label for='radio2'>★</label>
+                          <input id='radio3' type='radio' name='estrellas' value='3'>
+                          <label for='radio3'>★</label>
+                          <input id='radio4' type='radio' name='estrellas' value='2'>
+                          <label for='radio4'>★</label>
+                          <input id='radio5' type='radio' name='estrellas' value='1'>
+                          <label for='radio5'>★</label>
+                        </p>
+                    </form>
+                </div>
+                <div class='info'>
+                    <h2>$preg->titulo</h2>
+                    <p id='usuario'><b>Usuario:</b> $preg->usuario </p>
+                    <p id='fecha'><b>Fecha:</b> $preg->fecha</p>
+                    <p id='departamento'><b>Departamento:</b> $preg->categoria</p>
+                </div>
+                <div class='atributos'>
+                    <div class='stats'>
+                        <div class='iconos'>
+                            <button class='botones'><i class='fa-solid fa-thumbs-up'></i></button>
+                            <b class='nums'>$likes->like LIKE</b>
+                        </div>
+                        <div class='iconos'>
+                            <button class='botones'><i class='fa-brands fa-teamspeak'></i></button>
+                            <b class='nums'>$respuestas->respuestas RES</b>
+                        </div>
+                        <div class='iconos'>
+                            <button class='botones'><i class='fa-solid fa-eye'></i></button>
+                            <b class='nums'>$preg->vistos VISTO</b>
+                        </div>
+                        <a class='res' href='?accion=detalles&id=$preg->id_preg' onclick=\"actualizarVisto('$preg->id_preg')\">Responder</a>
+                    </div>
+                </div>
+            </div>";
+                  
+                `;
+            });
+
+            document.getElementsByClassName('preguntas').innerHTML = preguntas;
         
-    } catch (error) {
-        
+        } else if(respuesta.status === 401){
+            console.log('Pusiste la llave mal');
+        } else if(respuesta.status === 404){
+            console.log('La pelicula que buscas no existe');
+        } else {
+            console.log('Hubo un error y no sabemos que paso');
+        }
+
+    } catch(error){
+        console.log(error);
     }
 
 
 
  }
 
-
+ cargarPreguntas();
 
 
 
@@ -51,41 +121,3 @@
 
  
 
- 
- const cargarPeliculas = async() => {
-     try {
-         const respuesta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=192e0b9821564f26f52949758ea3c473&language=es-MX&page=${pagina}`);
-     
-         console.log(respuesta);
- 
-         // Si la respuesta es correcta
-         if(respuesta.status === 200){
-             const datos = await respuesta.json();
-             
-             let peliculas = '';
-             datos.results.forEach(pelicula => {
-                 peliculas += `
-                     <div class="pelicula">
-                         <img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
-                         <h3 class="titulo">${pelicula.title}</h3>
-                     </div>
-                 `;
-             });
- 
-             document.getElementById('contenedor').innerHTML = peliculas;
- 
-         } else if(respuesta.status === 401){
-             console.log('Pusiste la llave mal');
-         } else if(respuesta.status === 404){
-             console.log('La pelicula que buscas no existe');
-         } else {
-             console.log('Hubo un error y no sabemos que paso');
-         }
- 
-     } catch(error){
-         console.log(error);
-     }
- 
- }
- 
- cargarPeliculas();
--- www/PHP/pregunta.php
+++ www/PHP/pregunta.php
@@ -1,10 +1,10 @@
 <?php
     if (isset($_POST['id_preg'])) {
-<<<<<<< HEAD
+
          $dbh = connect();
-=======
+
         $dbh = connect();
->>>>>>> develop
+
         updateVisto($dbh, $_POST['id_preg']);
     }
 ?>
