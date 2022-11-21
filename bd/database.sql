74efa1d964a1c771715f30b301bf4761eca9c07e
Yeraybote <yeray.bote@ikasle.egibide.org>
2022-11-21 12:26:10 +0100

    INSERTS

--- bd/database.sql
+++ bd/database.sql
@@ -6,7 +6,7 @@ CREATE TABLE `USUARIO` (
   `email` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `contrasenia` varchar(20) NOT NULL,
   `imagen` longblob
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
 
 ALTER TABLE `USUARIO`
@@ -21,7 +21,7 @@ COMMIT;
 CREATE TABLE `CATEGORIA` (
   `id_cat` int NOT NULL,
   `nombre` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_spanish_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
 ALTER TABLE `CATEGORIA`
   ADD PRIMARY KEY (`id_cat`);
@@ -38,8 +38,7 @@ CREATE TABLE `PREGUNTA` (
   `archivo` longblob COMMENT 'subir archivo',
   `visto` INT NULL DEFAULT 0,
   `id_cat` int NOT NULL
-  
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
 ALTER TABLE `PREGUNTA`
   ADD PRIMARY KEY (`id_preg`),
@@ -59,7 +58,7 @@ CREATE TABLE `RESPUESTA` (
   `id_res` int NOT NULL,
   `descripcion` text CHARACTER SET utf8mb3 COLLATE utf8mb3_spanish_ci NOT NULL,
   `id_preg` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
 ALTER TABLE `RESPUESTA`
     ADD CONSTRAINT res_idpreg_fk
@@ -129,11 +128,11 @@ CREATE TABLE GUSTAR (
 
 
 /* INSERTAR DATOS BASE */
-INSERT INTO `CATEGORIA`(`nombre`) VALUES ('Informatica');
-INSERT INTO `CATEGORIA`(`nombre`) VALUES ('Medicina');
-INSERT INTO `CATEGORIA`(`nombre`) VALUES ('Ingenieria');
-INSERT INTO `CATEGORIA`(`nombre`) VALUES ('Jardineria');
-INSERT INTO `CATEGORIA`(`nombre`) VALUES ('Panaderia');
+INSERT INTO `CATEGORIA`(`nombre`) VALUES ('Aviacion');
+INSERT INTO `CATEGORIA`(`nombre`) VALUES ('Dirigibles');
+INSERT INTO `CATEGORIA`(`nombre`) VALUES ('Aeroespacio');
+INSERT INTO `CATEGORIA`(`nombre`) VALUES ('Parques aeronauticos');
+INSERT INTO `CATEGORIA`(`nombre`) VALUES ('Seguridad Aerea');
 
 INSERT INTO `USUARIO`(`nombre`, `apellidos`, `email`, `contrasenia`) VALUES ('Pedro','Gonzalez','pedro@gmail.com','pedro');
 INSERT INTO `USUARIO`(`nombre`, `apellidos`, `email`, `contrasenia`) VALUES ('Pepe','Fernandez','pepe@gmail.com','pepe');
@@ -142,17 +141,19 @@ INSERT INTO `USUARIO`(`nombre`, `apellidos`, `email`, `contrasenia`) VALUES ('Ma
 INSERT INTO `USUARIO`(`nombre`, `apellidos`, `email`, `contrasenia`) VALUES ('Kike','Garcia','kike@gmail.com','kike');
 INSERT INTO `USUARIO`(`nombre`, `email`, `contrasenia`) VALUES ('Albatxu', 'albatxu@gmail.com', 'Hola1234');
 
-INSERT INTO `PREGUNTA`(`titulo`, `detalle`, `id_cat`) VALUES ('¿Es correcto decir "yo y Santiago" o debemos decir "Santiago y yo"?','En una enumeración referida a personas, se aconseja situar el pronombre yo al final por razones de cortesía, pero no es lingüísticamente incorrecto que aparezca en primer lugar.','1');
-INSERT INTO `PREGUNTA`(`titulo`, `detalle`, `id_cat`) VALUES ('¿Se escribe "a gusto" o "agusto"?','Esta expresión, que significa ‘cómodamente’, ‘con gusto o placer’ o ‘según el gusto o deseo de alguien’, se escribe siempre en dos palabras: a gusto. No se considera válida la grafía agusto.','2');
-INSERT INTO `PREGUNTA`(`titulo`, `detalle`, `id_cat`) VALUES ('¿Cuál es la fórmula de saludo más adecuada, "buen día" o "buenos días"?','Como saludo matutino, la fórmula generalmente empleada en todo el ámbito hispanohablante es buenos días. Esta fórmula, única usada en España, alterna en el español de América con buen día, que está sobre todo extendida en el área rioplatense (Argentina, Paraguay y Uruguay).','3');
-INSERT INTO `PREGUNTA`(`titulo`, `detalle`, `id_cat`) VALUES ('¿Se escribe "no te rayes" o "no te ralles"?','El verbo que se usa en el habla coloquial juvenil con el sentido de ‘trastornar(se), volver(se) loco’ es rayar(se): No te rayes; Me estás rayando. El verbo rallar significa ‘desmenuzar algo con el rallador’: Necesito que me ralles un poco de queso para la lasaña.','4');
-INSERT INTO `PREGUNTA`(`titulo`, `detalle`, `id_cat`) VALUES ('¿Se escribe "Eres muy muy cruel" o "Eres muy, muy cruel"?','Construcciones como esta, en las que se duplica un elemento para aportar énfasis a lo expresado, se escriben sin coma: Eres muy muy cruel; Me gusta el café café; Hace mucho mucho tiempo.','5');
+INSERT INTO `PREGUNTA`(`titulo`, `detalle`, `id_cat`,`visto`) VALUES ('¿Qué es la regla Paretto y dónde podría aplicarse este princicpio?','No había conocido esta ley hasta el día de hoy, pero imagino que su aplicación en la meteorología aeronáutica tiene que ver con la utilización de una escala meteorológica menor (mesoescala e incluso microescala) que son aquellas escalas en la que están los fenómenos que afectan a la aeronavegación (tormentas, downburst, microburst, etc). Tienen una extensión y una duración menor que la escala sinóptica (serían el 20 de esa ley, pero generarían el 80% de los problemas que afectan a la navegación aérea).','1',7);
+INSERT INTO `PREGUNTA`(`titulo`, `detalle`, `id_cat`,`visto`) VALUES ('¿Qué problemas puede causar una tormenta en los alrededores de un aeropuerto?','Un avión en la aproximación final puede encontrar una fuerte ascendencia debida a la parte exterior del torbellino horizontal creado por rebote del "downburst". Quizá la reacción del piloto sea la de bajar el morro del avión, lo cual no puede ser más desaconsejable, ya que inmediatamente se encontrará la intensa corriente descendente y con tal presentación del avión, las consecuencias pueden ser fatales.','2',31);
+INSERT INTO `PREGUNTA`(`titulo`, `detalle`, `id_cat`,`visto`) VALUES ('¿Qué significa VNE? ¿Qué pasa si no se cumple?','No debe confundirse el "downburst" con el tornado; son fenómenos de extensión parecida y a veces efectos similares, pero hay una diferencia esencial: en el "downburst" las corrientes son descendentes, mientras que en el tornado se combinan ascendentes y en espiral. Acaso para la pequeña aviación revistan especial peligrosidad los procedentes de las tormentas secas por ser mas difíciles de localizar y en buena parte de los casos casi imposible identificar visualmente.','3',21);
+INSERT INTO `PREGUNTA`(`titulo`, `detalle`, `id_cat`,`visto`) VALUES ('¿Por qué es importante el pronóstico del viento en un aeropuerto?','La turbulencia por cortante de viento (o cizalladura del viento) se produce cuando hay un cambio brusco en la velocidad y/o dirección del viento. A grandes alturas este tipo de turbulencia se denomina CAT (Turbulencia en aire claro) y se asocia en general a la corriente en chorro. No se asocia a nubosidad de allí su nombre y por lo tanto no puede reconocerse a simple vista.','4',56);
+INSERT INTO `PREGUNTA`(`titulo`, `detalle`, `id_cat`,`visto`) VALUES ('¿Qué significa "alternate"?','La turbulencia mecánica se debe a los torbellinos que se generan en el aire al chocar con diferentes obstáculos en la superficie. Puede verse incrementada en el caso de aire inestable y también debido a la presencia de una barrera orográfica. Las ondas de montaña generan turbulencia moderada o severa y su manifestación visible son los altocúmulos lenticulares que se forman en esas ondas.','5',3);
 
-INSERT INTO `RESPUESTA`(`descripcion`,`id_preg`) VALUES ('El burro delante para que no se espante. Con este dicho u otros similares se suele censurar a quien, en una enumeración, se nombra a sí mismo en primer lugar, gesto que ya parecía descortés en tiempos de Gonzalo Correas: "Los arrieros siempre echan los asnos delante".',1);
-INSERT INTO `RESPUESTA`(`descripcion`,`id_preg`) VALUES ('No, no es incorrecto ni lo ha sido nunca, aunque las normas de urbanidad aconsejen mencionar primero al otro. De hecho, no es nada difícil encontrar, en prestigiosos autores de todas las épocas, ejemplos en los que el pronombre que designa al hablante es el primero de una serie, larga o corta, de elementos coordinados.',1);
-INSERT INTO `RESPUESTA`(`descripcion`,`id_preg`) VALUES ('Hay ocasiones, incluso, donde el orden adecuado viene determinado por el contexto y puede ser relevante no citarse en último lugar. Si alguien nos dice Las primeras en llegar a la meta fuimos yo, Eva y Ana, interpretaremos que nuestra interlocutora ha ganado la carrera; si se cita en segundo lugar, lo lógico es pensar que ha llegado en segunda posición. ',2);
-INSERT INTO `RESPUESTA`(`descripcion`,`id_preg`) VALUES ('Con los pronombres del plural parece que la libertad es mayor: resulta más natural decir nosotros y ellos que a la inversa, en lo que quizá influyan razones rítmicas o prosódicas. Por otra parte, si entre los elementos coordinados aparecen entes no animados, la norma de cortesía se deshace y lo normal es que la persona figure en primer lugar.',3);
-INSERT INTO `RESPUESTA`(`descripcion`,`id_preg`) VALUES ('Por tanto, ni las reglas linguísticas ni las de urbanidad se conculcan en ejemplos como los siguientes, en los que sería raro que el pronombre de primera persona apareciera en un lugar distinto al que ocupa: "¡Oh, mal haya yo y todo mi linaje"',4);
+INSERT INTO `RESPUESTA`(`descripcion`,`id_preg`) VALUES ('La regla o ley de Paretto (la Regla del 80/20) significa que el 20% de algo es esencial y el 80% es trivial. El 20% de los defectos causan el 80% de los problemas, el 20% del trabajo consume el 80% del tiempo y los recursos. La regla del 80/20 también se aplica a las ventas (el 20% de los clientes produce el 80% de los beneficios; o el 20% de los vendedores realiza el 80% de las ventas) o a cualquier otra cosa (el 20% del diario trae el 80% de las noticias importantes, o que el 20% de los empleados causan el 80% de los problemas).',1);
+INSERT INTO `RESPUESTA`(`descripcion`,`id_preg`) VALUES ('Las aeronaves que llegan o parten del aeropuerto podrían encontrarse con granizo, engelamiento, turbulencia e incluso en casos extremos con fuertes descendentes (microrráfagas o downburst: macroburst o microburst) que podrían tener graves consecuencias. Las llamadas en inglés "downburst" al llegar al suelo se extienden con violencia. No sólo se forman los temidos "downburst" en las fases de comienzo del estado de madurez de un proceso tormentoso, es decir, en el llamado "reventón" en muchos países de habla hispana, sino que también se pueden presentar en tormentas secas.',1);
+INSERT INTO `RESPUESTA`(`descripcion`,`id_preg`) VALUES ('VNE es la velocidad de nunca exceder. Se marca con una línea roja en muchos indicadores de velocidad aérea y varía en cada aeronave. Exceder VNE da lugar a una pérdida de control e incluso puede producir fallas estructurales. No es seguro volar en o cerca de VNE en condiciones de turbulencia.',2);
+INSERT INTO `RESPUESTA`(`descripcion`,`id_preg`) VALUES ('Cambios en la dirección o velocidad del viento tienen implicancias directas en las operaciones del aeródromo. De acuerdo a la dirección del viento se define qué cabecera de pista se empleará en las operaciones de despegue y aterrizaje. Además, las aeronaves tienen limitaciones respecto de la componente de viento cruzado (que a su vez varían si la pista está seca o mojada).',3);
+INSERT INTO `RESPUESTA`(`descripcion`,`id_preg`) VALUES ('Significa alternativa, o aeropuerto de alternativa. En los planes de vuelo se establecen de antemano a qué aeropuertos se va a dirigir la aeronave en caso de que el aeropuerto de destino se encuentre cerrado o en caso de que la aeronave tenga algún problema y no pueda llegar al aeropuerto de destino. El aeropuerto de alternativa a seleccionar para cualquier destino,deberá garantizar con la mayor probabilidad de éxito el aterrizaje en caso que el aeropuerto de destino se encuentre bajo mínimos, contemplando las limitaciones de techo y visibilidad (no de distancia de vuelo) de la categoría que reviste el piloto.',4);
+INSERT INTO `RESPUESTA`(`descripcion`,`id_preg`) VALUES ('Se usa el QNH. EL QNH es la presión al nivel del mar deducida de la existente en el aeródromo, considerando la atmósfera en condiciones estándar. La utilidad de esta presión de referencia se debe a que en las cartas de navegación y de aproximación a los aeródromos, las altitudes se indican respecto al nivel del mar. Con esta presión de referencia, al despegar o aterrizar el altímetro debería indicar la altitud real del aeródromo.',1);
+
 
 INSERT INTO `PREGUNTAR`(`id_usu`, `id_preg`) VALUES (1,1);
 INSERT INTO `PREGUNTAR`(`id_usu`, `id_preg`) VALUES (2,2);
@@ -165,6 +166,19 @@ INSERT INTO `RESPONDER`(`id_usu`, `id_res`) VALUES (2,2);
 INSERT INTO `RESPONDER`(`id_usu`, `id_res`) VALUES (3,3);
 INSERT INTO `RESPONDER`(`id_usu`, `id_res`) VALUES (4,4);
 INSERT INTO `RESPONDER`(`id_usu`, `id_res`) VALUES (5,5);
+
+INSERT INTO `VOTAR`(`id_usu`,`id_res`) VALUES (1,1);
+INSERT INTO `VOTAR`(`id_usu`,`id_res`) VALUES (2,1);
+INSERT INTO `VOTAR`(`id_usu`,`id_res`) VALUES (3,1);
+INSERT INTO `VOTAR`(`id_usu`,`id_res`) VALUES (1,2);
+INSERT INTO `VOTAR`(`id_usu`,`id_res`) VALUES (1,3);
+INSERT INTO `VOTAR`(`id_usu`,`id_res`) VALUES (2,2);
+
+INSERT INTO `GUSTAR`(`id_usu`,`id_preg`) VALUES (1,1);
+INSERT INTO `GUSTAR`(`id_usu`,`id_preg`) VALUES (2,3);
+INSERT INTO `GUSTAR`(`id_usu`,`id_preg`) VALUES (2,4);
+INSERT INTO `GUSTAR`(`id_usu`,`id_preg`) VALUES (4,2);
+INSERT INTO `GUSTAR`(`id_usu`,`id_preg`) VALUES (3,1);
 
 /* VISTA PARA LA VISUALIZACIÓN PREGUNTAS */
 CREATE VIEW vistaPreguntas AS
