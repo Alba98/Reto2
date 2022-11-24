362dbc5dd488d3b51059d060112236eb7eb8d74e
Yeraybote <yeray.bote@ikasle.egibide.org>
2022-11-24 13:49:29 +0100

    Siuuuuuuuuu

--- www/JS/perfil.js
+++ www/JS/perfil.js
@@ -135,6 +135,11 @@ async function cargarPreguntasUsuario(id_usuario) {
   }
 }
 
+function setValoracion(valoracion,num) {
+  if (valoracion == num) {
+      return "checked";
+  } else return "";
+}
 
 function cargarLayoutPregunta(datosPregunta) {
   let contenedorPregunta = document.getElementsByClassName("historial-preguntas")[0];
@@ -146,19 +151,19 @@ function cargarLayoutPregunta(datosPregunta) {
   <h2 class='titulousuario' id='titulousuario'>${datosPregunta.usuario}</h2>
   <img class='perfil' src='../RECURSOS/IMAGES/user.png' alt='Foto de perfil'>
   <form>
-      <p class='clasificacion'>
-          <input id='radio1' type='radio' name='estrellas' value='5'>
-          <label for='radio1'>★</label>
-          <input id='radio2' type='radio' name='estrellas' value='4'>
-          <label for='radio2'>★</label>
-          <input id='radio3' type='radio' name='estrellas' value='3'>
-          <label for='radio3'>★</label>
-          <input id='radio4' type='radio' name='estrellas' value='2'>
-          <label for='radio4'>★</label>
-          <input id='radio5' type='radio' name='estrellas' value='1'>
-          <label for='radio5'>★</label>
-      </p>
-  </form>
+  <p class='clasificacion'>
+      <input id='radio1' type='radio' name='estrellas' value='5' disabled ${setValoracion(datosPregunta.valoracion,5)}>
+      <label for='radio1'>★</label>
+      <input id='radio2' type='radio' name='estrellas' value='4' disabled ${setValoracion(datosPregunta.valoracion,4)}>
+      <label for='radio2'>★</label>
+      <input id='radio3' type='radio' name='estrellas' value='3' disabled ${setValoracion(datosPregunta.valoracion,3)}>
+      <label for='radio3'>★</label>
+      <input id='radio4' type='radio' name='estrellas' value='2' disabled ${setValoracion(datosPregunta.valoracion,2)}>
+      <label for='radio4'>★</label>
+      <input id='radio5' type='radio' name='estrellas' value='1' disabled ${setValoracion(datosPregunta.valoracion,1)}>
+      <label for='radio5'>★</label>
+  </p>
+</form>
 </div>
 <div class='info'>
   <h2>${datosPregunta.titulo}</h2> <br>
